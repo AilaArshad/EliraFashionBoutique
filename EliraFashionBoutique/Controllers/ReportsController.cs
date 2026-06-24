f6e65b14af68dcb1ae4907d5f4183b2bd0f3adcf
asmaismail001 <f2023065417@umt.edu.pk>
2026-06-24 16:17:09 +0500

    API Reports And API Screens

---
+++ EliraFashionBoutique/Controllers/ReportsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EliraFashionBoutique.Controllers;
+
+public class ReportsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
